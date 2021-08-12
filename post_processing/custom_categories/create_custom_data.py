e91f85908c1137141f752a59a19fa0fb62c47dbd
Théo <theo.vincent@eleves.enpc.fr>
2021-08-12 15:45:00 +0000

    Split residual correlations

--- post_processing/custom_categories/create_custom_data.py
+++ post_processing/custom_categories/create_custom_data.py
@@ -1,52 +1,68 @@
+from os import curdir
 import pandas as pd
 
-from website import CUSTOM_CATEGORIES_INDEX
+from website import CUSTOM_CATEGORIES, CUSTOM_CATEGORIES_INDEX, METHODS, TARGETS
 
 
 if __name__ == "__main__":
-    scores_residual = pd.DataFrame(pd.read_feather("data/all_categories/scores_residual.feather")).set_index(
-        ["main_category", "category", "algorithm"]
-    )
-    scores_residual.columns = pd.MultiIndex.from_tuples(
-        list(map(eval, scores_residual.columns.tolist())), names=["target", "fold", "metric"]
-    )
+    # scores_residual = pd.DataFrame(pd.read_feather("data/all_categories/scores_residual.feather")).set_index(
+    #     ["main_category", "category", "algorithm"]
+    # )
+    # custom_scores_residual = scores_residual.loc[
+    #     (
+    #         CUSTOM_CATEGORIES_INDEX.get_level_values("main_category"),
+    #         CUSTOM_CATEGORIES_INDEX.get_level_values("category"),
+    #     ),
+    #     :,
+    # ]
+    # custom_scores_residual.reset_index().to_feather("data/custom_categories/scores_residual.feather")
 
-    custom_scores_residual = scores_residual.loc[
-        (
-            CUSTOM_CATEGORIES_INDEX.get_level_values("main_category"),
-            CUSTOM_CATEGORIES_INDEX.get_level_values("category"),
-        ),
-        :,
-    ]
-    custom_scores_residual.columns = map(str, custom_scores_residual.columns.tolist())
-    custom_scores_residual.reset_index().to_feather("data/custom_categories/scores_residual.feather")
+    # scores_feature_importances = pd.DataFrame(
+    #     pd.read_feather("data/all_categories/scores_feature_importances.feather")
+    # ).set_index(["main_category", "category", "algorithm"])
+    # custom_scores_feature_importances = scores_feature_importances.loc[
+    #     (
+    #         CUSTOM_CATEGORIES_INDEX.get_level_values("main_category"),
+    #         CUSTOM_CATEGORIES_INDEX.get_level_values("category"),
+    #     ),
+    #     :,
+    # ]
+    # custom_scores_feature_importances.reset_index().to_feather(
+    #     "data/custom_categories/scores_feature_importances.feather"
+    # )
 
-    scores_feature_importances = pd.DataFrame(
-        pd.read_feather("data/all_categories/scores_feature_importances.feather")
-    ).set_index(["main_category", "category", "algorithm"])
-    scores_feature_importances.columns = pd.MultiIndex.from_tuples(
-        list(map(eval, scores_feature_importances.columns.tolist())), names=["target", "fold", "metric"]
-    )
+    # information = pd.DataFrame(pd.read_feather(f"data/all_categories/information.feather")).set_index(
+    #     ["main_category", "category"]
+    # )
+    # custom_information = information.loc[CUSTOM_CATEGORIES_INDEX]
+    # custom_information.reset_index().to_feather("data/custom_categories/information.feather")
 
-    custom_scores_feature_importances = scores_feature_importances.loc[
-        (
-            CUSTOM_CATEGORIES_INDEX.get_level_values("main_category"),
-            CUSTOM_CATEGORIES_INDEX.get_level_values("category"),
-        ),
-        :,
-    ]
-    custom_scores_feature_importances.columns = map(str, custom_scores_feature_importances.columns.tolist())
-    custom_scores_feature_importances.reset_index().to_feather(
-        "data/custom_categories/scores_feature_importances.feather"
-    )
+    for idx_target_row, target_row in enumerate(TARGETS):
+        for target_column in list(TARGETS.keys())[idx_target_row:]:
+            for method in METHODS:
+                for std_path in ["", "_std"]:
+                    path_correlations = f"data/all_categories/correlation/residual/{method}{std_path}_{target_row}_{target_column}.feather"
 
-    information = pd.DataFrame(pd.read_feather(f"data/all_categories/information.feather")).set_index(
-        ["main_category", "category"]
-    )
-    information.columns = pd.MultiIndex.from_tuples(
-        list(map(eval, information.columns.tolist())), names=["target", "information", "detail"]
-    )
+                    correlations = pd.read_feather(path_correlations).set_index(
+                        ["main_category", "category", "algorithm"]
+                    )
+                    custom_correlations = correlations.loc[
+                        (
+                            CUSTOM_CATEGORIES_INDEX.get_level_values("main_category"),
+                            CUSTOM_CATEGORIES_INDEX.get_level_values("category"),
+                        ),
+                        :,
+                    ]
+                    custom_correlations.reset_index().to_feather(
+                        f"data/custom_categories/correlation/residual/{method}{std_path}_{target_row}_{target_column}.feather"
+                    )
 
-    custom_information = information.loc[CUSTOM_CATEGORIES_INDEX]
-    custom_information.columns = map(str, custom_information.columns.tolist())
-    custom_information.reset_index().to_feather("data/custom_categories/information.feather")
+            path_number_participants = (
+                f"data/all_categories/correlation/residual/number_participants_{target_row}_{target_column}.feather"
+            )
+
+            number_participants = pd.read_feather(path_number_participants).set_index(["main_category", "category"])
+            custom_number_participants = number_participants.loc[CUSTOM_CATEGORIES_INDEX]
+            custom_number_participants.reset_index().to_feather(
+                f"data/custom_categories/correlation/residual/number_participants_{target_row}_{target_column}.feather"
+            )
