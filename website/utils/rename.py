741b0630c5ba2c871a2d26fda93970b6d9773bfc
Théo <theo.vincent@eleves.enpc.fr>
2021-08-13 15:03:48 +0000

    Add rename indexes

--- website/utils/rename.py
+++ website/utils/rename.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from website import MAIN_CATEGORIES, CATEGORIES, CUSTOM_CATEGORIES, ALGORITHMS
 
 
@@ -28,3 +30,24 @@ def rename(data, main_category=True, category=True, algorithm=True, index=True, 
 
         if algorithm:
             data.rename(columns=ALGORITHMS, level="algorithm", inplace=True)
+
+
+def rename_index(
+    indexes, main_category=True, category=True, algorithm=True, index=True, columns=True, custom_categories=True
+):
+    data = pd.DataFrame(None, index=indexes)
+
+    if main_category:
+        data.rename(index=MAIN_CATEGORIES, level="main_category", inplace=True)
+
+    if category:
+        for main_category in MAIN_CATEGORIES:
+            if custom_categories:
+                data.rename(index=CUSTOM_CATEGORIES[main_category], level="category", inplace=True)
+            else:
+                data.rename(index=CATEGORIES[main_category], level="category", inplace=True)
+
+    if algorithm:
+        data.rename(index=ALGORITHMS, level="algorithm", inplace=True)
+
+    return data.index
