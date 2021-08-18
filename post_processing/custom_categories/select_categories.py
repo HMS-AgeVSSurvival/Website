52965fa1150ed862c760242aced1dbcc64e9900c
Théo <theo.vincent@eleves.enpc.fr>
2021-08-18 15:28:04 +0000

    Change the way we select categories

--- post_processing/custom_categories/select_categories.py
+++ post_processing/custom_categories/select_categories.py
@@ -1,37 +1,35 @@
 import pandas as pd
 import numpy as np
 
+from post_processing import ALGORITHMS, TARGETS
 
-def objective_from_metrics(metrics):
-    list_scores = [metrics[("age", "test", "r2")] - metrics[("age", "test", "r2_std")]] * 6
+CATEGORIES_TO_SELECT = 30
 
-    for target in ["all", "cvd", "cancer"]:
-        list_scores.append(metrics[(target, "test", "c_index")] - metrics[(target, "test", "c_index_std")] - 0.5)
-        list_scores.append(
-            (metrics[(target, "test", "diff_c_index")] - metrics[(target, "test", "diff_c_index_std")]) * 25
-        )
 
-    return pd.Series(list_scores).sum()
+def objective_function(metrics):
+    best_scores = pd.Series(None, index=TARGETS, dtype=np.float32)
+    for target in TARGETS:
+        metric = "r2" if target == "age" else "diff_c_index"
+        best_scores.loc[target] = (
+            metrics.loc[(target, ALGORITHMS, "test", metric)]
+            - metrics.loc[(target, ALGORITHMS, "test", f"{metric}_std")].values
+        ).max()
+
+    return best_scores
 
 
 if __name__ == "__main__":
-    scores = pd.DataFrame(pd.read_feather("data/scores.feather")).set_index(["main_category", "category", "algorithm"])
+    scores = pd.DataFrame(pd.read_feather("data/all_categories/scores_residual.feather")).set_index(
+        ["main_category", "category"]
+    )
     scores.columns = pd.MultiIndex.from_tuples(
-        list(map(eval, scores.columns.tolist())), names=["target", "fold", "metric"]
+        list(map(eval, scores.columns.tolist())), names=["target", "algorithm", "fold", "metric"]
     )
 
-    scores_algorithm_indexed = scores.swaplevel(0, 2).swaplevel()
+    best_scores_counting_std = scores.apply(objective_function, axis="columns")
+    rank_scores = best_scores_counting_std.apply(lambda scores: scores.rank(ascending=False, na_option="bottom"))
+    rank_scores["age"] *= 3
 
-    list_objectives = []
-
-    for algorithm in scores_algorithm_indexed.index.get_level_values("algorithm").drop_duplicates():
-        list_objectives.append(
-            scores_algorithm_indexed.loc[algorithm].apply(
-                lambda metrics: objective_from_metrics(metrics), axis="columns"
-            )
-        )
-
-    objectives = pd.concat(list_objectives)
-    objectives.sort_values(ascending=False, inplace=True)
-
-    print(sorted(objectives.index.drop_duplicates()[:30].to_list()))
+    ranked_categories = rank_scores.sum(axis=1).sort_values().index
+    print(sorted(ranked_categories.values[:CATEGORIES_TO_SELECT]))
+    print(scores.loc[ranked_categories[CATEGORIES_TO_SELECT]].to_frame().T)
